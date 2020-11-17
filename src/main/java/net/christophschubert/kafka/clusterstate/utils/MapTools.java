0da4268994ca50d4c0200af37874aa40ab4418b5
Christoph Schubert <christoph.schubert@tu-dortmund.de>
2020-11-17 09:49:23 +0100

    Added JavaDoc, test, and improved null-value handling.

--- src/main/java/net/christophschubert/kafka/clusterstate/utils/MapTools.java
+++ src/main/java/net/christophschubert/kafka/clusterstate/utils/MapTools.java
@@ -14,12 +14,44 @@ import java.util.stream.Collectors;
 
 public class MapTools {
 
-    private static Logger logger = LoggerFactory.getLogger(MapTools.class);
+    private final static Logger logger = LoggerFactory.getLogger(MapTools.class);
 
+    /**
+     * Applies a function to each value of a map.
+     *
+     * @param map the input map
+     * @param valueMapper function to apply to each value of the map
+     * @param <K> type of map keys
+     * @param <V1> input type of the function
+     * @param <V2> output type of the function
+     * @return a new Map whose values have been computed from the input map
+     */
     public static <K,V1, V2> Map<K, V2> mapValues(Map<K, V1> map, Function<V1, V2> valueMapper) {
         return map.entrySet().stream()
                 .collect(Collectors.toMap(Map.Entry::getKey, e -> valueMapper.apply(e.getValue())));
     }
+
+    /**
+     * Applies a function to each value of a map and drops those entries for which null is returned.
+     *
+     * @param map the input map
+     * @param valueMapper function to apply to each value of the map
+     * @param <K> type of map keys
+     * @param <V1> input type of the function
+     * @param <V2> output type of the function
+     * @return a new Map whose values have been computed from the input map
+     */
+    public static <K,V1, V2> Map<K, V2> mapValuesDropNull(Map<K, V1> map, Function<V1, V2> valueMapper) {
+        final Map<K, V2> result = new HashMap<>();
+        map.forEach((k, v) -> {
+            final var res = valueMapper.apply(v);
+            if (res != null) {
+                result.put(k, res);
+            }
+        });
+        return result;
+    }
+
     /**
      * Filter a map to keep those key/value pairs whose keys satisfy a predicate.
      *
@@ -71,22 +103,46 @@ public class MapTools {
                 .collect(Collectors.toMap(e -> keyMapper.apply(e.getKey()), Map.Entry::getValue));
     }
 
+    /**
+     * Groups a list by a specific key.
+     *
+     * Entries for which {@code keyExtractor} returns null are skipped.
+     *
+     * @param input the input list
+     * @param keyExtractor function used to extract key
+     * @param <K> type of keys
+     * @param <V> type of values
+     * @return a map which contains the entries of the list, grouped by
+     */
     public static <K,V> Map<K, List<V>> groupBy(List<V> input, Function<V, K> keyExtractor) {
         final Map<K, List<V>> grouped = new HashMap<>();
         input.forEach(v -> {
             final K key = keyExtractor.apply(v);
-            grouped.computeIfAbsent(key, k -> new ArrayList<>());
-            grouped.get(key).add(v);
+            if (key != null) {
+                grouped.computeIfAbsent(key, k -> new ArrayList<>());
+                grouped.get(key).add(v);
+            }
         });
         return grouped;
     }
 
+    /**
+     * Constructs a map from a list: each entry in the list will be split into a key/value pair using the provided
+     * splitter function.
+     *
+     * Entries for which the splitter function returns a list whose size does not equal 2 will not be included in the map.
+     *
+     * @param input the input list
+     * @param splitter function used to construct key/value pair from entry of list
+     * @param <T> type of keys and values
+     * @return a map with entries for each line
+     */
     public static <T> Map<T, T> mapFromList(List<T> input, Function<T, List<T>> splitter) {
         final Map<T, T> result = new HashMap<>();
 
         for (T t : input) {
             final var list = splitter.apply(t);
-            if (list.size() == 2) {
+            if (list != null && list.size() == 2) {
                 result.put(list.get(0), list.get(1));
             } else {
                 logger.error("entry {} cannot be split into two parts, skipping it", t);
