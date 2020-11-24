84e8242ae7af98117b8d5ade0a23f106595b40f2
Christoph Schubert <christoph.schubert@tu-dortmund.de>
2020-11-24 09:32:32 +0100

    Added integration tests based on testcontainers.
    * new source set for integration tests
    
    * gradle tasks to run integration tests
    
    * first test case

--- src/intTest/java/net/christophschubert/kafka/clusterstate/Integration.java
+++ src/intTest/java/net/christophschubert/kafka/clusterstate/Integration.java
@@ -1,18 +1,13 @@
 package net.christophschubert.kafka.clusterstate;
 
 
-import org.apache.kafka.clients.admin.KafkaAdminClient;
-
-import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
-import java.util.List;
 import java.util.Properties;
 import java.util.concurrent.ExecutionException;
 
 
 public class Integration {
-
 
     public static void main(String[] args) throws IOException, ExecutionException, InterruptedException {
         Properties cloudProperties = new Properties();
